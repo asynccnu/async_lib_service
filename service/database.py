3fae11733a600c3fdb1dbeed0270a62d8c3d07f9
Darren <darren@Darren-MBP.lan>
2019-05-19 02:09:48 +0800

    fix: add mongodb auth

--- service/database.py
+++ service/database.py
@@ -3,9 +3,13 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGOHOST = os.getenv('MONGOHOST') or 'localhost'
 MONGOPORT = int(os.getenv('MONGOPORT') or '27017')
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
 
 async def db_setup():
-    client = AsyncIOMotorClient(MONGOHOST, MONGOPORT)
+    # client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
+    mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+    client = AsyncIOMotorClient(mongo_uri)
     attention = client['attention']
     attentioncol = attention['attentiondb']
     return attention 
